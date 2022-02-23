318fa7686b6911f0541f5d0de411c914199ab5a6
Andre Avelino da Silva <andreavelinosilva@hotmail.com>
2022-02-23 19:51:47 -0300

    User insert

--- src/main/java/com/aastechnology/course/resources/UserResource.java
+++ src/main/java/com/aastechnology/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.aastechnology.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.aastechnology.course.entities.User;
 import com.aastechnology.course.services.UserService;
@@ -18,15 +22,24 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
-	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	@GetMapping // Anotation para recuperar dados do banco 
+	public ResponseEntity<List<User>> findAll(){ // metodo para retornar todos os usuarios do banco de dados
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = "/{id}") // metodo para retornar um usuario especifico pelo id.
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
 	
 }
